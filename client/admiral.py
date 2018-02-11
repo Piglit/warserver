da0ba242248de6e1a7ce6adb457bf36d720afc65
oren <oren@oren.oren>
2018-02-11 16:57:34 +0100

    fixes: fog of war, settings
    Dome bugfixes after Alpha-Test

--- client/admiral.py
+++ client/admiral.py
@@ -22,8 +22,7 @@ terrain_types = {
 	6:    "Crossroads",               
 }
 
-#PRIVILEGE_LEVEL = "admiral"
-PRIVILEGE_LEVEL = "gm"
+PRIVILEGE_LEVEL = "admiral"
 privilege_flags = {	#binary flags!
 	"gm-admiral":	3,
 	"gm":		2,
