5401b87446e16d9c45686125db23e4758019379b
oren <oren@oren.oren>
2018-02-15 18:34:35 +0100

    improved starter
    better startup messages
    pyro is optional now (but still needed for gui)
    quick single argument load

--- core/warserver.py
+++ core/warserver.py
@@ -2,24 +2,50 @@
 import configparser
 import argparse
 import pickle
+import sys
 
 import engine
 import artemis_connector
-import pyro_connector
+
+try:
+	import pyro_connector
+except ImportError:
+	PYRO = False
+else:
+	PYRO = True
 
 
 
-if __name__ == "__main__":
+def single_argument_load():
+	if len(sys.argv) == 2:
+		#handles drag n drop file
+		arg = sys.argv[1]
+		if not arg.startswith("-"):
+			#TODO write to readme: sav and cfg files must not start with -
+			if arg.endswith(".sav"):
+				start_game_from_save(arg) 
+			if arg.endswith(".cfg"):
+				start_game_with_config(arg) 
+	return False
+
+
+def multiple_argument_load(args):
+	if args.load:
+		start_game_from_save(args.load)
+	else:
+		start_game_with_config(args.config)
+
+def start_game_from_save(filename):
+	with open(filename, "rb") as file:
+		engine.game=pickle.load(file)
+	print("loading saved game " + filename)
+	engine.game._start_from_loaded_game()
+
+
+def start_game_with_config(filename):
 	config = configparser.ConfigParser()
-	parser = argparse.ArgumentParser(description='Starts the Artemis WarServer.') 
-	parser.add_argument('--config', '-c', type=open, default='default.cfg', help='Load config file') 
-	parser.add_argument('--load', '-l', type=argparse.FileType("rb"), help='Load saved game') 
-	parser.add_argument('--ip', type=str, help='your ip address or hostname, where clients can connect to the server') 
-	parser.add_argument('--nameserver', type=str, help='hostname or ip address, where a pyro naming server can be found or should be started (if the address belongs to this computer)')
-	args = parser.parse_args()
-	#if throws an exception if files can not be found
-
-	config.read_file(args.config)
+	with open(filename, "r") as file:
+		config.read_file(file)
 	settings = {}
 	for key in config["int"]:
 		settings[key] = config["int"].getint(key)
@@ -27,21 +53,28 @@ if __name__ == "__main__":
 		settings[key] = config["float"].getfloat(key)
 	for key in config["bool"]:
 		settings[key] = config["bool"].getboolean(key)
+	print("starting new game with configuration from '" + filename + "'")
+	engine.game=engine.Game(settings)
 
-	if args.load:
-		engine.game=pickle.load(args.load)
-		engine.game._start_from_loaded_game()
-	else:
-		print("This is the python Artemis warserver.")
-		print("Configuration loaded from file "+str(args.config.name))
-		engine.game=engine.Game(settings)	
-		#engine.start_game=(config['original'])	
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Starts the Artemis WarServer.') 
+	parser.add_argument('--config', '-c', type=str, default='default.cfg', help='Load config file') 
+	parser.add_argument('--load', '-l', type=str, help='Load saved game') 
+	parser.add_argument('--ip', type=str, help='your ip address or hostname, where clients can connect to the server') 
+	parser.add_argument('--nameserver', type=str, help='hostname or ip address, where a pyro naming server can be found or should be started (if the address belongs to this computer)')
+	args = parser.parse_args()
+
+	if not single_argument_load():
+		multiple_argument_load(args)
+		#both throws an exception if files can not be found
 	artemis_connector.start_server()
 	#json_connector.test()	
-	print(80*'-')
-	pyro_connector.start_pyro_server(ip=args.ip, host=args.nameserver)
-	print(80*'-')
-	print("You may start the admiral or game-master client on this or some other machine in your network.")
+	if PYRO: 
+		print(80*'-')
+		pyro_connector.start_pyro_server(ip=args.ip, host=args.nameserver)
+		print(80*'-')
+		print("You may start the admiral or game-master client on this or some other machine in your network.")
+	else:
+		print("Warning: Pyro connector could not be loaded.")
 
-
-
+	print("Ready to play!")
